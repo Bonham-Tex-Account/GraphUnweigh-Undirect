e5c38ec1d9d6783114eeea68eaeb99306d168ae2
Tex Bonham <texwasbad@gmail.com>
2021-02-28 10:39:17 -0800

    Graph Class

---
+++ Graph.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnweighedAndUndirected
+{
+    public class Vertex<T>
+    {
+        public T value { get; set; }
+        public List<Vertex<T>> Neighbor {get;set;}
+        public int NeighborCount => Neighbor.Count;
+        public Vertex(T Value)
+        {
+            value = Value;
+        }
+    }
+    public class Graph<T>
+    {
+        public List<Vertex<T>> Vertrcies { get; private set; }
+        public int VerteciesCount => Vertrcies.Count;
+        public Graph()
+        {
+
+        }
+        public void Add()
+        {
+
+        }
+        public bool Remove()
+        {
+
+        }
+        public bool AddEdege()
+        {
+
+        }
+        public bool RemoveEdge()
+        {
+
+        }
+        public Vertex<T> Search()
+        {
+
+        }
+
+    }
+}
